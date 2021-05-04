34c24dec91e88ccd3ed4e6518dc8be14c3bae588
flakster <carlos.santamaria@gmail.com>
2021-05-04 17:46:32 -0500

    Format a weather description based on the info fetched from the API

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const url = "http://api.weatherstack.com/current?access_key=b1ddcca8a8bf51f3c7189b83026d7a6b&query=bogota";
 
-request({ url }, ( error, response ) => {
-  const data = JSON.parse(response.body)
-  console.log(data.current);
+request({ url:url, json:true }, ( error, response ) => {
+  const {temperature, feelslike} =  response.body.current;
+  console.log(`It is currently ${temperature} degrees out and it feels like ${feelslike} out`); 
 });
